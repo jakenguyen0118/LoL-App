b1f7860702222886a1de288f28aeae569a0e0b4a
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-08 12:09:16 -0400

    added build

---
+++ build/precache-manifest.3b857d9a747d36aba822a296fe0ebbfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795d4d1dd5c2c6688efcf6407313c53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c42fa629c301d3dfe3e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "adaac8eaa623c5711ec5",
+    "url": "/static/css/main.25274478.chunk.css"
+  },
+  {
+    "revision": "5c42fa629c301d3dfe3e",
+    "url": "/static/js/2.c6343c79.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.c6343c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaac8eaa623c5711ec5",
+    "url": "/static/js/main.605459d9.chunk.js"
+  },
+  {
+    "revision": "d77370c004225c7d4e6c",
+    "url": "/static/js/runtime-main.29043849.js"
+  }
+]);
